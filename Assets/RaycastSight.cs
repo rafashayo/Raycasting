3d848cc26a49659ba150376251a071c2afd027dd
rafashayo <49433616@est.ort.edu.ar>
2025-09-18 10:36:57 -0300

    Raycasting Agent

---
+++ Assets/RaycastSight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastSight : MonoBehaviour
+{
+
+    [SerializeField] Transform originTR;
+    [SerializeField] float rayLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originTR = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitInfo;
+        if(Physics.Raycast(originTR.position, originTR.forward, out hitInfo, rayLength))
+        {
+            Debug.Log(hitInfo.collider.name);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Color color = Color.red;
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(originTR.position, originTR.position + originTR.forward * rayLength);
+    }
+}
---
+++ Assets/Scripts/AgentScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentScript : MonoBehaviour
+{
+    NavMeshAgent agent;
+    [SerializeField] Transform[] patrolPoints;
+    [SerializeField] bool isPatrolling = true;
+    [SerializeField] float arrivalDistance = 1;
+    [SerializeField] Animator anim;
+    [SerializeField] float velocity;
+    [SerializeField] Transform currentDestination;
+    [SerializeField] int currentPatrolPointIndex;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentDestination = patrolPoints[0];
+        currentPatrolPointIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(agent.hasPath && agent.remainingDistance <= arrivalDistance)
+        {
+            if(currentPatrolPointIndex < patrolPoints.Length -1)
+            {
+                currentPatrolPointIndex++;
+            }
+            else
+            {
+                currentPatrolPointIndex = 0;
+            }
+
+            currentDestination = patrolPoints[currentPatrolPointIndex];
+        }
+        agent.destination = currentDestination.position;
+        velocity = agent.velocity.magnitude;
+        anim.SetFloat("Speed",velocity);
+    }
+}
